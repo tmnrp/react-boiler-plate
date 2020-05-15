eed3e5c05a49fbce998145c41675b4012c4636c7
tmnrp <tony.venom007@gmail.com>
2020-05-15 16:25:08 +0530

    Async support

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# portfolio
-Portfolio site
+# changeme
+changeme
--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,10 @@
+import "./App.scss";
 import React from "react";
 
 const App = () => {
-  return <div data-testid="myid">Hello World</div>;
+	return <div data-testid="myid">Hello World</div>;
 };
+
+const myAsync = async () => {};
 
 export default App;
